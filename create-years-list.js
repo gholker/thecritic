397acd8d73f2f91700960cacbb6bf4e1dfb312f6
Graham Holker <graham.holker@gmail.com>
2016-03-12 12:01:13 -0500

    alexa intents and utterances

---
+++ create-years-list.js
@@ -0,0 +1,30 @@
+
+for (var i = 1980; i <= 2016; i++) {
+    console.log("" + i);
+}
+
+for (var i = 80; i < 100; i++) {
+    console.log("nineteen " + i);
+}
+
+for (var i = 10; i < 17; i++) {
+    console.log("twenty " + i);
+}
+
+console.log("two thousand");
+console.log("two thousand one");
+console.log("two thousand two");
+console.log("two thousand three");
+console.log("two thousand four");
+console.log("two thousand five");
+console.log("two thousand six");
+console.log("two thousand seven");
+console.log("two thousand eight");
+console.log("two thousand nine");
+console.log("two thousand ten");
+console.log("two thousand eleven");
+console.log("two thousand twelve");
+console.log("two thousand thirteen");
+console.log("two thousand fourteen");
+console.log("two thousand fifteen");
+console.log("two thousand sixteen");
